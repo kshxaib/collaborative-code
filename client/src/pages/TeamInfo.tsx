7bbcdcc95b17ae26b3ad40a975a0a0facd71d663
Khan Shoaib <Khanshoaibishtiyak@gmail.com>
2025-02-26 20:52:18 +0530

    final

--- client/src/pages/TeamInfo.tsx
+++ client/src/pages/TeamInfo.tsx
@@ -1,5 +1,5 @@
 import { motion } from 'framer-motion';
-import { useEffect } from 'react';
+// import { useEffect } from 'react';
 import { useInView } from 'react-intersection-observer';
 
 const fadeInUp = {
